8fffaef7ed3c5df5f9b9641642856f8c77632283
Nahtanm <nahtanmcs0019@gmail.com>
2025-01-20 16:38:04 -0300

    Project Created

---
+++ src/test/java/com/project/java_intensivo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.java_intensivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
